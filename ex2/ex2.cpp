db8d7f56ef301b221e35c79dc4e393114bd80ed8
raquel.milheiro2000@gmail.com <raquel.milheiro2000@gmail.com>
2021-11-08 17:17:43 +0000

    ex2 e 5

---
+++ ex2/ex2.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+
+using namespace std;
+
+int main(int argc,char **argv){
+    char x;
+
+    if(argc != 3){
+        cout << "Error: Should write <input filename> <output filename>" << endl;
+        return 0; 
+    }
+  
+    //0 argumento -> ./a.out
+    //1 argumento -> read file
+    //2 argumento -> writable file
+    
+    ifstream ifs(argv[1]); //read from a file
+    ofstream ofs(argv[2]); //write from a file
+
+    while(ifs.get(x)){ //read from character to character and write to a file
+        ofs<<x;
+    }
+
+    ofs.close();
+    return 0;
+}
