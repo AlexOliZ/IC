32e70733bb10a31e19544e7704bb2e144b8be0bb
AlexOliZ <alexandretomas@ua.pt>
2021-11-18 14:39:27 +0000

    ex10 done

--- ex10.cpp
+++ ex10.cpp
@@ -1,3 +1,6 @@
+//g++ ex10.cpp -lsndfile
+// ./a.out sample01.wav
+
 #include <iostream>
 #include <string>
 #include <fstream>
@@ -7,75 +10,62 @@
 //#include "matplotlibcpp.h"
 #include <cmath>
 #include <math.h>
+#include <cstring>
 
 //using namespace plt = matplotlibcpp;
 using namespace std;
 
 int main(int argc ,char *argv[])
 {
-    if(argc < 2)
-    {
-        printf("Missing args\n");
-        return -1;
+    if(argc != 2){
+        std::cout << "USE IS: ./ex8 <samplename.wav>"<< std::endl;
+        return 1;
     }
-    string filename = argv[1];
+    char *inFileName = (char*)malloc(sizeof(char*));
+    std::cout << argv[1] << std::endl;
+    strcpy(inFileName,argv[1]);
 
-    //cout << "input filename: ";
-    //cin >> filename;
+    double *buf,*buf_r;
+    int num_items,num;
+    double sum_r=0,sum=0;
+    double SNR;
+    SNDFILE *inFile,*outFile;
+    SF_INFO inFileInfo;
+    string filename = argv[1];
 
     const char* path = "./Wav files-20211025/";
     filename = path + filename;
     path = filename.c_str();
 
-    SF_INFO sfinfo;
-    SNDFILE* snd_file;
-    FILE *out;
+    inFile = sf_open(path, SFM_READ, &inFileInfo);
+    int channels = inFileInfo.channels;
 
-    sfinfo.format = 0;
-    snd_file = sf_open(path,SFM_READ,&sfinfo);
+    num_items = inFileInfo.frames*channels;
 
-    if(snd_file == NULL)
-    {
-        printf("Failed to open the file\n");
-    }    
-
-    int num_channels, num, num_items, f, sr,c, i , j;
-    double *buf;
-    f = sfinfo.frames;
-    sr = sfinfo.samplerate;
-    c = sfinfo.channels;
-
-    num_items = f*c;
-
-    printf("frames=%d\n",f);
-    printf("samplerate=%d\n",sr);
-    printf("channels=%d\n",c);
-
+    /* Allocate space for the data to be read, then read it. */
     buf = (double *) malloc(num_items*sizeof(double));
-    num = sf_read_double(snd_file,buf,num_items);
-
-}
-
-void make_copy(char* filename){
-    audioFile.load ("sample01.wav");
-    std::ofstream ofs ("output.txt");
-
-    std::map <double, int> histogram;
-
-    int channels = audioFile.getNumChannels();
-    int numSamples = audioFile.getNumSamplesPerChannel();
-    double x;
+    num = sf_read_double(inFile,buf,num_items);
+    buf_r = (double *) malloc(num_items*sizeof(double));
+    double max_abs_error = 0;
     //Δ=|t−s|/(λ−1) 
-    for(int channel = 0; channel < channels; channel++){
-        for (int i = 0; i < numSamples; i++)
-        {
-        double currentSample = audioFile.samples[channel][i];
-        audioFile.samples[channel][i] = 0.25*floor(currentSample/0.25+0.5);
-        //ofs << currentSample << "<-->" << audioFile.samples[channel][i] << "<-->" << currentSample - audioFile.samples[channel][i] <<endl;
+    //4bits => 2 / (2⁴-1) = 0,1333
+    for (int i = 0; i < num; i += channels)
+    {
+        for (int j = 0; j < channels; ++j){
+            buf_r[i+j] = 0.1333*floor(buf[i+j]/0.1333+0.5);
+            sum += pow(buf_r[i+j],2);
+            sum_r += pow(buf[i+j]-buf_r[i+j],2);
+            if(pow(buf[i+j]-buf_r[i+j],2) > max_abs_error)
+                max_abs_error = pow(buf[i+j]-buf_r[i+j],2);
         }
     }
-    //audioFile.setBitDepth (8);
+    SNR = 10*log(sum/sum_r);
 
-    audioFile.save ("output.wav");
+    printf("SUM: %f\n",sum);
+    printf("SUM_R: %f\n",sum_r);
+    printf("SNR: %f\n",SNR);
+    printf("MAX ABS ERROR: %f\n",max_abs_error);
 
-    return 0;
+
+
+}
