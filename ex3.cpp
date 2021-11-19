674a445a6ba38f49e9f9231199108dd8acb44045
ledropoureiro <psloureiro@ua.pt>
2021-11-19 13:12:00 +0000

    ex3 mudanças

--- ex3.cpp
+++ ex3.cpp
@@ -7,24 +7,19 @@ using namespace std;
 
 int main(int argc ,char *argv[])
 {
-    if(argc < 2)
+    if(argc < 3)
     {
         printf("Missing args\n");
         return -1;
     }
     string filename = argv[1];
 
-    //cout << "input filename: ";
-    //cin >> filename;
-
     const char* path = "./Wav files-20211025/";
     filename = path + filename;
     path = filename.c_str();
 
     SF_INFO sfinfo;
-    SNDFILE* snd_file;
-
-    FILE *out;
+    SNDFILE* snd_file,*outfile;
 
     sfinfo.format = 0;
     snd_file = sf_open(path,SFM_READ,&sfinfo);
@@ -34,7 +29,7 @@ int main(int argc ,char *argv[])
         printf("Failed to open the file\n");
     }    
 
-    int num_channels, num, num_items, *buf, f, sr,c, i , j;
+    int num_channels, num, num_items, *buf, f, sr,c;
 
     f = sfinfo.frames;
     sr = sfinfo.samplerate;
@@ -52,20 +47,15 @@ int main(int argc ,char *argv[])
     sf_close(snd_file);
     printf("Read %d items\n",num);
 
-    /* Write the data to filedata.out. */
     path = "./";
     
     filename = argv[2];
     filename = path + filename;
     path = filename.c_str();
 
-    out = fopen(path,"w");
-    for (i = 0; i < num; i += c)
-    {
-        for (j = 0; j < c; ++j)
-            fprintf(out,"%d ",buf[i+j]);
-        fprintf(out,"\n");
-    }
+    outfile = sf_open(path,SFM_WRITE,&sfinfo);
+    num = sf_write_int(outfile,buf,num_items);
+    printf("Write %d items\n",num);
 
-    fclose(out);
+    sf_close(outfile);
 }
